cb69a4258d892d2a8398e2cd5d5642a9e77c5afe
Randall Rouse <randall.g.rouse@gmail.com>
2024-07-17 19:08:04 -0700

    chore: renamed Procfile

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', AppRouter)
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(`${__dirname}/client/build/index.html`))
-    })
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, 'client/build')))
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+//     })
+// }
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`));
